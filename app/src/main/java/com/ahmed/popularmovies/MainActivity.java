3e21a3e89defb3f5c442dc02d85780aad65c354b
Ahmed Nasir <ahmednasir91@gmail.com>
2016-05-02 01:32:31 +0500

    Moved API Key to Build config, added menu.

--- app/src/main/java/com/ahmed/popularmovies/MainActivity.java
+++ app/src/main/java/com/ahmed/popularmovies/MainActivity.java
@@ -3,6 +3,9 @@ package com.ahmed.popularmovies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -22,13 +25,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private static final String THE_MOVIE_DB_URL = "http://api.themoviedb.org/";
-    private static final String API_KEY = ""; // Add API Key here
+    private static final String API_KEY = BuildConfig.THE_MOVIE_DB_API_KEY; // Add API Key here
 
     private static final String TAG = MainActivity.class.getName();
 
     private TheMovieDBGateway mTheMovieDBGateway;
     private MovieAdapter mMovieAdapter;
     private List<Movie> movieList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         initGateway();
 
-        loadMovies();
+        showPopularMovies();
 
         initUI();
     }
@@ -48,24 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         GridView moviesGV = ((GridView) findViewById(R.id.movies_gv));
         moviesGV.setAdapter(mMovieAdapter);
-    }
-
-    private void loadMovies() {
-        Call<Response<List<Movie>>> responseCall = mTheMovieDBGateway.popularMovies(API_KEY);
-        responseCall.enqueue(new Callback<Response<List<Movie>>>() {
-            @Override
-            public void onResponse(Call<Response<List<Movie>>> call, retrofit2.Response<Response<List<Movie>>> response) {
-                showMoviesList(response.body().getResults());
-
-                Log.i(TAG, "Done");
-            }
-
-            @Override
-            public void onFailure(Call<Response<List<Movie>>> call, Throwable t) {
-                Log.e(TAG, "There was an error downloading movies list.");
-                Toast.makeText(MainActivity.this, "An error occured downloading list.", Toast.LENGTH_LONG).show();
-            }
-        });
     }
 
     private void showMoviesList(List<Movie> movies) {
@@ -82,5 +68,49 @@ public class MainActivity extends AppCompatActivity {
         mTheMovieDBGateway = retrofit.create(TheMovieDBGateway.class);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_popular_movies:
+                showPopularMovies();
+                break;
+            case R.id.action_top_rated_movies:
+                showTopRatedMovies();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showTopRatedMovies() {
+        Call<Response<List<Movie>>> responseCall = mTheMovieDBGateway.topRatedMovies(API_KEY);
+        responseCall.enqueue(movieListDownloadedCallback);
+    }
+
+    private void showPopularMovies() {
+        Call<Response<List<Movie>>> responseCall = mTheMovieDBGateway.popularMovies(API_KEY);
+        responseCall.enqueue(movieListDownloadedCallback);
+    }
+
+    private Callback<Response<List<Movie>>> movieListDownloadedCallback = new Callback<Response<List<Movie>>>() {
+        @Override
+        public void onResponse(Call<Response<List<Movie>>> call, retrofit2.Response<Response<List<Movie>>> response) {
+            showMoviesList(response.body().getResults());
+
+            Log.i(TAG, "Done");
+        }
+
+        @Override
+        public void onFailure(Call<Response<List<Movie>>> call, Throwable t) {
+            Log.e(TAG, "There was an error downloading movies list.");
+            Toast.makeText(MainActivity.this, "An error occured downloading list.", Toast.LENGTH_LONG).show();
+        }
+    };
 }
